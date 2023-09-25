9bd49da63c7e98cf08fd914b9c33d93e14a6fd7c
vivekjaiswal20003 <vickyguddu12@gmail.com>
2023-09-25 17:21:16 +0530

    contains dummy products

---
+++ dummy_products.py
@@ -0,0 +1,96 @@
+
+# Dummy product data
+dummy_products = [
+    {
+        "Product name": "Laptop",
+        "Product price": 999,
+        "Product available quantity": 8
+    },
+    {
+        "Product name": "Bluetooth Speaker",
+        "Product price": 79,
+        "Product available quantity": 20
+    },
+    {
+        "Product name": "Wireless Headphones",
+        "Product price": 149,
+        "Product available quantity": 15
+    },
+    {
+        "Product name": "Digital Camera",
+        "Product price": 299,
+        "Product available quantity": 12
+    },
+    {
+        "Product name": "Smartwatch",
+        "Product price": 199,
+        "Product available quantity": 10
+    },
+    {
+        "Product name": "Fitness Tracker",
+        "Product price": 89,
+        "Product available quantity": 25
+    },
+    {
+        "Product name": "Drone",
+        "Product price": 399,
+        "Product available quantity": 6
+    },
+    {
+        "Product name": "Bluetooth Earbuds",
+        "Product price": 69,
+        "Product available quantity": 30
+    },
+    {
+        "Product name": "External Hard Drive",
+        "Product price": 129,
+        "Product available quantity": 18
+    },
+    {
+        "Product name": "Wireless Mouse",
+        "Product price": 29,
+        "Product available quantity": 40
+    },
+    {
+        "Product name": "Air Purifier",
+        "Product price": 199,
+        "Product available quantity": 7
+    },
+    {
+        "Product name": "Coffee Maker",
+        "Product price": 79,
+        "Product available quantity": 15
+    },
+    {
+        "Product name": "Robot Vacuum Cleaner",
+        "Product price": 299,
+        "Product available quantity": 9
+    },
+    {
+        "Product name": "Bluetooth Car Kit",
+        "Product price": 39,
+        "Product available quantity": 35
+    },
+    {
+        "Product name": "Portable Projector",
+        "Product price": 249,
+        "Product available quantity": 11
+    },
+    {
+        "Product name": "Wireless Charger",
+        "Product price": 29,
+        "Product available quantity": 50
+    },
+    {
+        "Product name": "Gaming Console",
+        "Product price": 499,
+        "Product available quantity": 4
+    },
+    {
+        "Product name": "Fitness Equipment Set",
+        "Product price": 599,
+        "Product available quantity": 3
+    }
+]
+
+
